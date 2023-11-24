3d1d21b1baa731cf5346ff281907fccf2a7e86e6
Varun Thakore <vrnthakore@gmail.com>
2023-11-24 20:04:26 +0530

    Update ecdsa

--- src/ecdsa.rs
+++ src/ecdsa.rs
@@ -28,15 +28,14 @@ mod test {
     use bellpepper_core::test_cs::TestConstraintSystem;
     use crypto_bigint::{Encoding, U256};
     use ff::Field;
-    use halo2curves::{
-        secp256k1::{Fp, Fq, Secp256k1Affine},
-        CurveAffine,
-    };
+    use halo2curves::secp256k1::{Fp, Fq, Secp256k1Affine};
     use rand::thread_rng;
     use std::ops::{Mul, Neg};
 
-    fn sign(msg: Fq, priv_key: Fq) -> (Fq, Fq) {
+    fn sign(msg: Fq, priv_key: Fq) -> (Secp256k1Affine, Fq) {
         let mut rng = thread_rng();
+        let n =
+            U256::from_be_hex("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141");
         let g = Secp256k1Affine::generator();
 
         let k = Fq::random(&mut rng);
@@ -45,37 +44,60 @@ mod test {
         let k_inv = k_inv.unwrap();
 
         let r: Secp256k1Affine = g.mul(k).into();
-        let r_x = Fq::from_repr(r.x.to_bytes());
+        let r_x = Fq::from_repr(
+            U256::from_le_bytes(r.x.to_bytes())
+                .add_mod(&U256::ZERO, &n)
+                .to_le_bytes(),
+        );
         assert!(bool::from(r_x.is_some()));
         let r_x = r_x.unwrap();
 
         let s = k_inv * (msg + priv_key * r_x);
 
-        (r_x, s)
+        (r, s)
     }
 
-    fn get_points(rx: Fq, msg: Fq) -> (Secp256k1Affine, Secp256k1Affine) {
+    fn get_points(r: Secp256k1Affine, msg: Fq) -> (Secp256k1Affine, Secp256k1Affine) {
+        let n =
+            U256::from_be_hex("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141");
+
         let g = Secp256k1Affine::generator();
 
-        let r_x = Fp::from_repr(rx.to_bytes());
-        assert!(bool::from(r_x.is_some()));
-        let r_x = r_x.unwrap();
+        let r_q = Fq::from_repr(
+            U256::from_le_bytes(r.x.to_bytes())
+                .add_mod(&U256::ZERO, &n)
+                .to_le_bytes(),
+        );
+        assert!(bool::from(r_q.is_some()));
+        let r_q = r_q.unwrap();
 
-        let r_y = (r_x.square() * r_x + Fp::from(7)).sqrt();
-        assert!(bool::from(r_y.is_some()));
-        let r_y = r_y.unwrap();
-        let r = Secp256k1Affine::from_xy(r_x, r_y);
-        assert!(bool::from(r.is_some()));
-        let r = r.unwrap();
+        let r_q_inv = r_q.invert();
+        assert!(bool::from(r_q_inv.is_some()));
+        let r_q_inv = r_q_inv.unwrap();
 
-        let r_x_inv = Fq::from_repr(r_x.to_bytes()).unwrap().invert();
-        assert!(bool::from(r_x_inv.is_some()));
-        let r_x_inv = r_x_inv.unwrap();
-
-        let t: Secp256k1Affine = r.mul(r_x_inv).into();
-        let u: Secp256k1Affine = g.mul(r_x_inv * msg).neg().into();
+        let t: Secp256k1Affine = r.mul(r_q_inv).into();
+        let u: Secp256k1Affine = g.mul(r_q_inv * msg).neg().into();
 
         (t, u)
+    }
+
+    fn verify(
+        scalar: U256,
+        t: Secp256k1Affine,
+        u: Secp256k1Affine,
+        pub_key: Secp256k1Affine,
+    ) -> bool {
+        let scalar = Fq::from_repr(scalar.to_le_bytes());
+        assert!(bool::from(scalar.is_some()));
+        let scalar = scalar.unwrap();
+
+        let st: Secp256k1Affine = t.mul(scalar).into();
+        let pub_key_calc: Secp256k1Affine = (st + u).into();
+
+        let x_is_same = pub_key.x == pub_key_calc.x;
+        let y_is_same = pub_key.y == pub_key_calc.y;
+
+        x_is_same & y_is_same
     }
 
     #[test]
@@ -86,11 +108,11 @@ mod test {
 
             let msg = Fq::random(&mut rng);
             let priv_key = Fq::random(&mut rng);
-            let (r_x, s) = sign(msg, priv_key);
+            let (r, s) = sign(msg, priv_key);
 
             let pub_key: Secp256k1Affine = g.mul(priv_key).into();
 
-            let (t, u) = get_points(r_x, msg);
+            let (t, u) = get_points(r, msg);
 
             let mut cs = TestConstraintSystem::<Fp>::new();
 
@@ -110,7 +132,7 @@ mod test {
 
             let scalar = U256::from_le_bytes(s.to_bytes());
 
-            let verify = verify_eff(
+            let out = verify_eff(
                 &mut cs.namespace(|| "verify"),
                 scalar,
                 t_alloc,
@@ -120,9 +142,67 @@ mod test {
             .unwrap();
             let _ = Boolean::enforce_equal(
                 &mut cs.namespace(|| "verify == true"),
-                &verify,
+                &out,
                 &Boolean::constant(true),
             );
+
+            let out_exp = verify(scalar, t, u, pub_key);
+            assert!(out_exp == out.get_value().unwrap());
+
+            assert!(cs.is_satisfied());
+            assert_eq!(cs.num_constraints(), 3389);
+        }
+    }
+
+    #[test]
+    fn test_scalar_false() {
+        for _ in 0..100 {
+            let mut rng = thread_rng();
+            let g = Secp256k1Affine::generator();
+
+            let msg = Fq::random(&mut rng);
+            let priv_key = Fq::random(&mut rng);
+            let (r, _s) = sign(msg, priv_key);
+
+            let pub_key: Secp256k1Affine = g.mul(priv_key).into();
+
+            let (t, u) = get_points(r, msg);
+
+            let mut cs = TestConstraintSystem::<Fp>::new();
+
+            let pub_key_alloc = AllocatedAffinePoint::alloc_affine_point(
+                &mut cs.namespace(|| "alloc pub key"),
+                pub_key.x,
+                pub_key.y,
+            )
+            .unwrap();
+
+            let t_alloc =
+                AllocatedAffinePoint::alloc_affine_point(&mut cs.namespace(|| "alloc t"), t.x, t.y)
+                    .unwrap();
+            let u_alloc =
+                AllocatedAffinePoint::alloc_affine_point(&mut cs.namespace(|| "alloc u"), u.x, u.y)
+                    .unwrap();
+
+            let scalar = U256::from_le_bytes(Fq::random(&mut rng).to_bytes());
+
+            let out = verify_eff(
+                &mut cs.namespace(|| "verify"),
+                scalar,
+                t_alloc,
+                u_alloc,
+                pub_key_alloc,
+            )
+            .unwrap();
+            let _ = Boolean::enforce_equal(
+                &mut cs.namespace(|| "verify == true"),
+                &out,
+                &Boolean::constant(false),
+            );
+
+            let out_exp = verify(scalar, t, u, pub_key);
+            assert!(out_exp == out.get_value().unwrap());
+
             assert!(cs.is_satisfied());
             assert_eq!(cs.num_constraints(), 3389);
         }
